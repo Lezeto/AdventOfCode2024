3829099829f2a65e4c9a956731a3b024915f28d3
Lezeto <nicolas0vf@gmail.com>
2024-12-19 19:02:52 -0300

    day 7

---
+++ desafio7_2.js
@@ -0,0 +1,37 @@
+const fs = require("node:fs");
+var texto1= fs.readFileSync("/Users/nicol/Documents/estudiandin/2024/Advent of Code/7input.txt",{encoding: "utf8", flag: "r"});
+let lista=texto1.split("\r\n");
+lista = lista.map (x => x.split(": "));
+let suma=0;
+function concatenar(a,b){
+    return Number(String(a)+String(b));
+}
+
+function esPosible(listax){
+    let resultado= Number(listax[0]);
+    let listaB=listax[1].split(" ");
+    listaB= listaB.map (x=> Number(x))
+    let listaA=[];
+    let listaAA=[];
+    listaA.unshift(listaB.shift());
+    while (listaB.length>0){
+        for (let n=0; n<listaA.length;n++){
+            listaAA.unshift(listaA[n]+listaB[0]);
+            listaAA.unshift(listaA[n]*listaB[0]);
+            listaAA.unshift(concatenar(listaA[n],listaB[0]));
+        }
+        listaA=[...listaAA];
+        listaAA=[];
+        listaB.shift();
+    }
+    if (listaA.includes(resultado)){return true}else{return false}
+}
+
+for (let i=0; i<lista.length; i++){
+    if (esPosible(lista[i])){
+        suma+=Number(lista[i][0]);
+        console.log("la suma ahora es: "+ suma + ", se ha analizado " + i*100/lista.length + " %");
+    }
+}
+
+console.log(suma);
